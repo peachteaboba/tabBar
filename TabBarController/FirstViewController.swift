ad586446c158fd7e18c01e212ec4f277fc2aa8c7
Andy Feng <anndyfeng1@gmail.com>
2016-08-08 09:03:23 -0700

    first commit

--- TabBarController/FirstViewController.swift
+++ TabBarController/FirstViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("FirstViewController viewDidLoad")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print("FirstViewController viewWillAppear")
     }
 
 
---
+++ TabBarController/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  TabBarController
+//
+//  Created by Andy Feng on 8/8/16.
+//  Copyright © 2016 Andy Feng. All rights reserved.
+//
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("SecondViewController viewDidLoad")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        print("SecondViewController viewWillAppear")
+    }
+    
+    
+}
+
